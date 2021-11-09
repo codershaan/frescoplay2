aeeac435f4a6d86a477ecb53d635081644a68a0f
codershaan <44260483+codershaan@users.noreply.github.com>
2021-11-09 17:31:19 +0430

    Update SegmentController.test.js

--- test/controllers/SegmentController.test.js
+++ test/controllers/SegmentController.test.js
@@ -13,6 +13,8 @@ describe('Segment Controller', function() {
       .expect(200)
       .end(function(err, result) {
         if (err) {
+           console.log('S g',err);
+
           done(err);
         } else {
           result.body.length.should.be.aboveOrEqual(0);
@@ -20,5 +22,113 @@ describe('Segment Controller', function() {
         }
       });
   });
+  it('fetch /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .get('/segment/fetch')
+      .send()
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+           console.log('S f',err);
+
+          done(err);
+        } else {
+          
+          done();
+        }
+      });
+  });
+
+  it('create /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .put('/segment/create')
+      .send({})
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          console.log('S cr',err);
+
+          done(err);
+        } else {
+          
+
+          done();
+        }
+      });
+  });
+
+    it('update /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .put('/segment/update/:1')
+      .send({})
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+           console.log('S up',err);
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+
+    it('delete /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete('/segment/delete/:1')
+      .send({})
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          console.log('S de',err);
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+
+    it('deleteAll /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete('/segment/deleteAll')
+      .send()
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          console.log('S dA',err);
+
+          done(err);
+        } else {
+          
+
+          
+          done();
+        }
+      });
+  });
+
+    it('search /segment', function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .get('/segment/findOne/:1')
+      .send()
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          console.log('S F',err);
+
+          done(err);
+        } else {
+          
+
+          done();
+        }
+      });
+  });
+
 
 });
